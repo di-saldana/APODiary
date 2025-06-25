511973eca6cfd6b28d60372e6d4ec4bc2f0f1bc7
di-saldana <dsl42@alu.ua.es>
2025-06-25 17:54:11 -0400

    Initial Commit

---
+++ APODiary/APODiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  APODiaryApp.swift
+//  APODiary
+//
+//  Created by Dianelys Saldaña on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct APODiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APODiaryTests/APODiaryTests.swift
@@ -0,0 +1,16 @@
+//
+//  APODiaryTests.swift
+//  APODiaryTests
+//
+//  Created by Dianelys Saldaña on 6/25/25.
+//
+
+import Testing
+
+struct APODiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
